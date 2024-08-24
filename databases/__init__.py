1d80548bc93e833565acb7b65176737268de43fd
uni <7pluto8@gmail.com>
2024-08-24 13:55:05 +0900

    fix conflict remove prefect_duckdb

--- databases/__init__.py
+++ databases/__init__.py
@@ -3,22 +3,26 @@ from prefect_duckdb import DuckDBConnector
 from prefect.variables import Variable
 from prefect.blocks.system import Secret
 
+import duckdb
 import dotenv
 
 import os
 # .envファイルの内容を環境変数として読み込む
 dotenv.load_dotenv()
 
-secret_block = Secret.load("motheducktoken")
-if not secret_block:
+motherduck_token = Secret.load("motheducktoken").get()
+if not motherduck_token:
     motherduck_token = os.getenv("MOTHERDUCK_TOKEN")
-else:
-    motherduck_token = secret_block.get()
+
+
+# def get_connection():
+#     duckdb_connector = DuckDBConnector(
+#         database=f"md:?motherduck_token={motherduck_token}",
+#     ).load("motherduck")  # blockname
+
+#     return duckdb_connector
 
 
 def get_connection():
-    duckdb_connector = DuckDBConnector(
-        database=f"md:?motherduck_token={motherduck_token}",
-    ).load("motherduck")  # blockname
-
-    return duckdb_connector
+    connection = duckdb.connect(f"md:?motherduck_token={motherduck_token}")
+    return connection

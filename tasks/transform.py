02bbad66cbc3827c2e10551c45b9525b9c7921b2
uni <7pluto8@gmail.com>
2024-10-06 04:42:32 +0900

    use cli command

--- tasks/transform.py
+++ tasks/transform.py
@@ -3,18 +3,24 @@ from prefect import task
 from prefect_dbt.cli.commands import DbtCoreOperation
 from prefect_dbt.cli.commands import trigger_dbt_cli_command
 
+from pathlib import Path
+
+# プロジェクトのルートディレクトリを取得
+PROJECT_ROOT = Path(__file__).parent.parent
+DBT_PROJECT_DIR = PROJECT_ROOT / "dbt_project"
 
 @task
 def dbt_build_task():
-    #trigger_dbt_cli_command(
-        # command="dbt deps", profiles_dir="dbt_project",
-        # project_dir="dbt_project",
-    # )
-    DbtCoreOperation(
-        commands=["dbt deps"],
+    trigger_dbt_cli_command(
+        command="dbt deps",
+        profiles_dir=DBT_PROJECT_DIR,
         project_dir="dbt_project",
-        profiles_dir="dbt_project",
-    ).run()
+    )
+    # DbtCoreOperation(
+    #     commands=["dbt deps"],
+    #     project_dir="dbt_project",
+    #     profiles_dir="dbt_project",
+    # ).run()
 
 @task(retries=0)
 def transform_data_with_dbt() -> str:

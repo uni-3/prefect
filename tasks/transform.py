31d520a3ad5be51d7e09f0b66bff6cb397ff3463
uni <7pluto8@gmail.com>
2024-10-06 04:16:53 +0900

    add install deps

--- tasks/transform.py
+++ tasks/transform.py
@@ -1,9 +1,18 @@
 # dbt task
 from prefect import task
 from prefect_dbt.cli.commands import DbtCoreOperation
+from prefect_dbt.cli.commands import trigger_dbt_cli_command
+
+
+@task
+def dbt_build_task():
+    trigger_dbt_cli_command(
+        command="dbt deps", project_dir="./dbt_project",
+    )
 
 @task(retries=0)
 def transform_data_with_dbt() -> str:
+    dbt_build_task()
     res = DbtCoreOperation(
         commands=["dbt run --target prod"],
         project_dir="dbt_project",

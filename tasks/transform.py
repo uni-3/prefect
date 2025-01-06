834b9e94c807f7b8770aa78712689be2f41ad322
uni <7pluto8@gmail.com>
2025-01-07 02:18:11 +0900

    install deps in transform task and update makefile

--- tasks/transform.py
+++ tasks/transform.py
@@ -1,17 +1,28 @@
 from prefect import task
 from prefect_dbt.cli.commands import DbtCoreOperation, DbtCliProfile
 from prefect_dbt.cli.configs.bigquery import BigQueryTargetConfigs
+from prefect_dbt.cli.commands import trigger_dbt_cli_command
+
 from prefect_dbt.cli.configs import TargetConfigs
 
 
 @task(retries=0)
 def transform_data_with_dbt(profile_name: str = "blog", project_dir: str = "dbt_project", configs: TargetConfigs = BigQueryTargetConfigs.load("dbt-free-bigquery")) -> str:
     """dbt実行"""
+    # install dbt deps with prefect_dbt
+
     dbt_cli_profile = DbtCliProfile(
         name=profile_name,
         target="prod",
         target_configs=configs,
     )
+
+    trigger_dbt_cli_command(
+        command="dbt deps",
+        project_dir=project_dir,
+        profiles_dir=project_dir,
+    )
+
     dbt_init = DbtCoreOperation(
         commands=["dbt run"],
         dbt_cli_profile=dbt_cli_profile,

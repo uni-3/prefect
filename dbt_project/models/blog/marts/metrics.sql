b11d62d01ef0cfb9f70164482241b70520d59ed1
uni <31564310+uni-3@users.noreply.github.com>
2025-05-14 08:44:49 +0900

    To submodule dbt (#18)
    * remove dbt packages
    
    * add submodule
    
    * add option for submodule

--- dbt_project/models/blog/marts/metrics.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        description="サイトごとの日付別pv imp click.",
-    )
-}}
-select p.*, c.* except (data_date, url)
-from {{ ref("int_pv") }} as p
-left join {{ ref("int_imp_click") }} as c on p.d = c.data_date and p.page_location = c.url

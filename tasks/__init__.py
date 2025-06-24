216f473b3aad714d73acc060df0909a213e3b1c7
uni <7pluto8@gmail.com>
2025-06-25 01:12:20 +0900

    update

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,4 @@
-from .blog_content import transform_and_load_blogs_content
+from .blog_content import load 
 from .load_github import load_github_issues_task
 from .pokemon import get_pokemon_task
 from .transform import transform_task
